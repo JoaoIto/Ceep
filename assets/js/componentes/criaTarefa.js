5c7f5a6b205b8b4c7a26d6afb84af17292e4c3ea
João <joaovictorpfr@gmail.com>
2021-10-28 13:30:30 -0300

    add new files and changes

---
+++ assets/js/componentes/criaTarefa.js
@@ -0,0 +1,45 @@
+import { carregaTarefa } from './carregaTarefa.js'
+import BotaoConclui from './concluiTarefa.js'
+import BotaoDeleta from './deletaTarefa.js'
+ 
+
+export const handleNovoItem = (evento) => {
+    evento.preventDefault()
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'))||[]
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const dados = { 
+        valor,
+        dataFormatada
+    }
+
+    const tarefasAtualizadas = [...tarefas, dados]
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefasAtualizadas))
+
+    input.value = " "
+
+    carregaTarefa()
+    
+}
+
+export const Tarefa = ({ valor, dataFormatada }) => {
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+   
+    return tarefa
+
+}
