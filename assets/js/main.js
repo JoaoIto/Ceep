7e5de1cf5fcf754906fa675151d71e8416a2f2b8
JoaoIto <joaovictorpfr@gmail.com>
2023-04-25 08:20:09 -0300

    add new project version ts

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import {handleNovoItem} from './components/criaTarefa.js'
-import {carregaTarefa} from './components/carregaTarefa.js'
-const novaTarefa = document.querySelector('[data-form-button]')
-
-novaTarefa.addEventListener('click', handleNovoItem)
-
-carregaTarefa()
