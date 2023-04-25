ed2fcd19bd6239a2f5b540d65bce922c15871933
JoaoIto <joaovictorpfr@gmail.com>
2023-04-25 09:29:58 -0300

    layout base

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        screenBlue: "#44578a"
+      }
+    },
   },
   plugins: [],
 }
